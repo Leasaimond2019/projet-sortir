09eee5daf754aa88de519bbeadbc98cea61adb61
Léasticot <lea.saimond2018@campus-eni.fr>
2019-10-22 10:31:45 +0200

    fix error imported

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SortieRepository")
