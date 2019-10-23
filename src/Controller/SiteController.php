4b6a64a3e2b527df25bc3093d99f77ff3c1de73e
Léasticot <lea.saimond2018@campus-eni.fr>
2019-10-23 12:10:35 +0200

    Add controller Site
    parce que Kevs est un vieux type

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Site;
+use App\Form\SiteType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    /**
+     * @Route("/site", name="site")
+     */
+    public function index()
+    {
+        return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/manageSite", name="manage_site")
+     */
+    public function manage()
+    {
+        // récupérer les sites depuis la base de données
+        $sitesRepo = $this->getDoctrine()->getRepository(Site::class);
+        $sites = $sitesRepo->findAll();
+
+        return $this->render('sortie/manage.html.twig', [
+            'sites' => $sites,
+        ]);
+    }
+
+    /**
+     * @Route("/manageSite/{id}", name="manageID_site")
+     */
+    public function manageSiteById(Request $request){
+        // récupérer les sites depuis la base de données
+        $sitesRepo = $this->getDoctrine()->getRepository(Site::class);
+        $sites = $sitesRepo->findAll();
+
+        $site= $this->getDoctrine()->getRepository(Site::class)->find($request->attributes->get('id'));
+
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        return $this->render('sortie/manage.html.twig', [
+            'sites' => $sites,
+            'siteForm'=>$form->createView(),
+            'id'=>$request->attributes->get('id'),
+        ]);
+    }
+}
--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -62,36 +62,5 @@ class SortieController extends AbstractController
             "sortieForm" => $sortieForm->createView()
         ]);
     }
-    /**
-     * @Route("/manageSite", name="manage_site")
-     */
-    public function manage()
-    {
-        // récupérer les sites depuis la base de données
-        $sitesRepo = $this->getDoctrine()->getRepository(Site::class);
-        $sites = $sitesRepo->findAll();
 
-        return $this->render('sortie/manage.html.twig', [
-            'sites' => $sites,
-        ]);
-    }
-    /**
-     * @Route("/manageSite/{id}", name="manageID_site")
-     */
-    public function manageSiteById(Request $request){
-        // récupérer les sites depuis la base de données
-        $sitesRepo = $this->getDoctrine()->getRepository(Site::class);
-        $sites = $sitesRepo->findAll();
-
-        $site= $this->getDoctrine()->getRepository(Site::class)->find($request->attributes->get('id'));
-
-        $form = $this->createForm(SiteType::class, $site);
-        $form->handleRequest($request);
-
-        return $this->render('sortie/manage.html.twig', [
-            'sites' => $sites,
-            'siteForm'=>$form->createView(),
-            'id'=>$request->attributes->get('id'),
-        ]);
-    }
 }
